aef9a42860bc8ec001bbbe725d156e6b242a4a54
davood <davoodkhany@gmail.com>
2021-10-28 23:47:40 +0330

    Card underscore pakage

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -5,15 +5,23 @@
                 <!-- Blog entries-->
                 <div class="col-lg-8">
                     <!-- Nested row for non-featured blog posts-->
+               <div class="col-lg-12">
+                            <Card></Card>
+                </div>
                     <div class="row">
                         <div class="col-lg-6">
-                            <Card v-for="article in articles" :key="article.id"></Card>
+                            <Card></Card>
+                            <Card></Card>
+                        </div>
+                        <div class="col-lg-6">
+                            <Card></Card>
+                            <Card></Card>
                         </div>
                     </div>
                     <!-- Pagination-->
                     <nav aria-label="Pagination">
                         <hr class="my-0">
-                        <ul class="pagination justify-content-center my-4">
+                        <ul class="my-4 pagination justify-content-center">
                             <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
                             <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
                             <li class="page-item"><a class="page-link" href="#!">2</a></li>
@@ -27,13 +35,13 @@
                 <!-- Side widgets-->
                 <div class="col-lg-4">
                     <!-- Search widget-->
-                    <div class="card mb-4">
+                    <div class="mb-4 card">
                         <searchbox></searchbox>               
                     </div>
                     <!-- Categories widget-->
                         <Categories></Categories>
                     <!-- Side widget-->
-                    <div class="card mb-4">
+                    <div class="mb-4 card">
                         <div class="card-header">Side Widget</div>
                         <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
                     </div>
