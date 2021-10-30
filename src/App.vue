02fdcccffdcffad146b81cda8a27be6dabff3f3b
davood <davoodkhany@gmail.com>
2021-10-30 21:51:09 +0330

    template update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
-<template>
-  <Navbar></Navbar>
-  <Headerblog></Headerblog>
-  <Content></Content>
-  <Footerblog></Footerblog>
-</template>
-
-
-<script>
-import Navbar from "./components/Navbar-bog.vue";
-import Headerblog from "./components/Header-blog.vue";
-import Content from "./components/Content.vue";
-import Footerblog from "./components/Footer-blog.vue";
-
-export default {
-  components:{
-    Navbar,
-    Headerblog,
-    Content,
-    Footerblog,
-  },
- data(){
-   return{
-
-   }
- }
-}
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
 </script>
+
+
+
+<template>
+
+
+
+
+
+
+
+
+</template>
+
