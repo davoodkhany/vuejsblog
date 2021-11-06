1fd54a5f01bd8512a4ab6416b560e0ecbe88626a
davood <davoodkhany@gmail.com>
2021-11-06 22:47:41 +0330

    Slot vue js Template

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="alert alert-success" role="alert">
+    <div v-if="$slots.title">
+      <slot name="title">
+        <h2>Default page</h2>
+      </slot>
+    </div>
     <slot></slot>
-    <h2>My name Is Dvaood</h2>
+    <hr />
+    <slot name="description"></slot>
   </div>
 </template>
