692b6a33c3150f52a889a9827014c1c9963e87cb
davood <davoodkhany@gmail.com>
2021-11-09 21:43:03 +0330

    defineAsyncComponent »شطغ مخیهدل

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,42 @@
 <template>
-        <my-nav :page="currentPage" @changePage="currentPage = $event"></my-nav>
+  <my-nav :page="currentPage" @changePage="currentPage = $event"></my-nav>
 
-        <component :is="currentPage"></component>
+  <component :is="currentPage"></component>
 
-        <my-footer></my-footer>
-
-        
-
+  <my-footer></my-footer>
 </template>
 
 <script>
-
-  import Nav from './components/layouts/Nav.vue';
-  import Footer from './components/layouts/Footer.vue';
-
-  import Home from './components/pages/Home.vue'
-  import Contact from './components/pages/Contact.vue'
-  import About from './components/pages/About.vue'
+import Nav from "./components/layouts/Nav.vue";
+import Footer from "./components/layouts/Footer.vue";
 
 
-  export default {
-    components : {
-      'my-nav' :Nav,
-      'my-footer' : Footer,
-      Home,
-      Contact,
-      About
-    },
-    data() {
-      return {
-        currentPage : 'home'
-      }
-    }
-  }
+
+import { defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    "my-nav": Nav,
+    "my-footer": Footer,
+    'home': defineAsyncComponent(() => import('./components/pages/Home.vue')),
+    'contact': defineAsyncComponent(() =>
+      import('./components/pages/Contact.vue')
+    ),
+    'about': defineAsyncComponent(() => import('./components/pages/About.vue')),
+  },
+  data() {
+    return {
+      currentPage: "home",
+    };
+  },
+};
 </script>
 
-<style>
-.card_shop .flex_column_table{
-    background: white;
-    padding: 20px;
-    border-radius: 24px;
-}
 
+<style>
+.card_shop .flex_column_table {
+  background: white;
+  padding: 20px;
+  border-radius: 24px;
+}
 </style>
