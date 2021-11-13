1d677ca7d75a61d533f2462577e937f075fac128
davood <davoodkhany@gmail.com>
2021-11-13 23:27:48 +0330

    Reactive vue method

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,19 +1,20 @@
 <template>
     <h2>About Page</h2>
-    <h1>{{name}}</h1>
+    <h1>{{name.firstName}}</h1>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {ref,reactive} from 'vue'
 export default {
     setup(){
 
-        const name = ref('hi davood')
-            setTimeout(() => {
-                console.log('run')
-                name.value = "hi ali"
-                console.log('run');
-            }, 2000);
+        const name = reactive({
+            firstName:'davood',
+            lastName: 'khany'
+        })
+        setTimeout(() => {
+            name.firstName = 'ali'
+        }, 2000);
 
         return {
             name
